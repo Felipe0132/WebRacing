7cdf756bdbe5f71fcd1f9344b686e9ffbfe8c743
Felipe Silva <felipesiilvaa2006@gmail.com>
2026-05-19 22:58:04 -0300

    AJeitando controllers

--- src/backend/controller/corridaController.js
+++ src/backend/controller/corridaController.js
@@ -2,18 +2,43 @@ const CorridaService = require('../service/corridaService')
 
 const corridaService = new CorridaService()
 
-const create = async(req, res, next) => {
+const create = async(req, res) => {
     try{
-        const corridaDTO = req.body; // Extrai o bruto da requisicao
-
-        const corridaRecord = await corridaService.create(corridaDTO) // Passa na regra de negocios
-
-        return res.status(201).json({ corrida: corridaRecord });
-    }catch(error){
-        return res.status(400).json({ message: corrida.message });
+        const record = await corridaService.create(req.body) // Cria no Service
+        return res.status(201).json(record)
+    }catch(e){
+        return res.status(400).json({error: e.message})
     }
 }
 
+const buscar = async(req, res) => {
+    try{
+        const filtros = req.query;
+        const records = await corridaService.search(filtros); 
+        return res.json(records)
+    }catch(e){
+        return res.status(404).json({ error: e.message });
+    }
+}
+
+const update = async(req, res) => {
+    try{
+        const records = await corridaService.update(req.params.id, req.body)
+        return res.json(records)
+    }catch(e){
+        return res.status(400).json({error: e.message})
+    }
+}
+const remove = async(req, res) => {
+    try{
+        const result = await corridaService.delete(req.params.id)
+        return res.json(result)
+    }catch(e){
+        return res.status(404).json({error: e.message})
+    }
+}
+
+
 module.exports = {
-  create
+  create, buscar, update, remove  
 };
