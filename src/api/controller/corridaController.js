10db6da41df4a026c469cf3ed243634de9a47835
Felipe Silva <felipesiilvaa2006@gmail.com>
2026-05-20 00:11:28 -0300

    Ajeitando GET

--- src/api/controller/corridaController.js
+++ src/api/controller/corridaController.js
@@ -13,7 +13,28 @@ const create = async(req, res) => {
 
 const buscar = async(req, res) => {
     try{
-        const filtros = req.query;
+        const { nome, data, local, distancia } = req.query
+        const filtros = {}
+
+        if (nome) {
+            filtros.nome = new RegExp(nome, 'i')
+        }
+
+        if (local) {
+            filtros.nome = new RegExp(local, 'i')
+        }
+
+        if (distancia) {
+            filtros.distancias = Number(distancia)
+        }
+
+        if(data){
+            const dataFormatada = new Date(data)
+
+            filtros.data = dataFormatada
+        }
+
+
         const records = await corridaService.busca(filtros); 
         return res.json(records)
     }catch(e){
